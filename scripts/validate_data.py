7dfcd881f8f0f83b52726203bf4a7811c4918949
Alessio <alessio.giacomel@kcl.ac.uk>
2023-08-31 22:29:19 +0200

    validate data functions made working. TODO: change print with logs and add testing

--- scripts/validate_data.py
+++ scripts/validate_data.py
@@ -9,7 +9,7 @@ from pathlib import Path
 import sys
 import hashlib
 
-def file_hash(filename):
+def file_hash(filename: str) -> str:
     """ Get byte contents of file `filename`, return SHA1 hash
 
     Parameters
@@ -32,7 +32,7 @@ def file_hash(filename):
     return hashlib.sha1(filename.read_bytes()).hexdigest()
     
 
-def validate_data(data_directory):
+def validate_data(data_directory: str) -> None:
     """ Read ``data_hashes.txt`` file in `data_directory`, check hashes
 
     Parameters
@@ -50,7 +50,7 @@ def validate_data(data_directory):
         If hash value for any file is different from hash value recorded in
         ``data_hashes.txt`` file.
     """
-    # Read lines from ``data_hashes.txt`` file.
+    # Read lines from ``hash_lists.txt`` file.
     # Split into SHA1 hash and filename
     # Calculate actual hash for given filename.
     # If hash for filename is not the same as the one in the file, raise
@@ -61,7 +61,8 @@ def validate_data(data_directory):
     if not data_directory.is_dir():
         raise NotADirectoryError(f'{data_directory} is not a directory')
     # Read the data_hashes.txt file
-    data_hashes = data_directory / 'data_hashes.txt'
+    data_hashes = next(data_directory.rglob('group-*/hash_list.txt'), None)
+    print(data_hashes)
     if not data_hashes.exists():
         raise FileExistsError(f'{data_hashes} does not exist')
     # Loop through the lines of the data_hashes.txt file. The file contents are composed as:
@@ -71,7 +72,11 @@ def validate_data(data_directory):
         hash2 = file_hash(data_directory / filename)
         if hash1 != hash2:
             raise ValueError(f'Hash mismatch for {filename}: '
-                             f'{hash1} != {hash2}')
+                             f'{hash1} != {hash2}')                     
+        else:
+            print(f'Hash match for {filename}: {hash1} == {hash2}')
+    return
+
 
 def main():
     # This function (main) called when this file run as a script.
